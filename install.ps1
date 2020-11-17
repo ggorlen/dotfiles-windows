a773c18c981f729cc7b590977d568d65760b26b2
ggorlen <gsgorlen@gmail.com>
2020-11-17 13:03:58 -0800

    add packages

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-if ((which cinst) -eq $null) {
+if ((Get-Command cinst).Path -eq $null) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
