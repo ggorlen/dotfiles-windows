f0f5fb75d1b42e22f7b67afc2480586d035db16b
ggorlen <gsgorlen@gmail.com>
2020-11-28 07:49:58 -0800

    remove vcxsrv

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -24,7 +24,33 @@ function dl { Set-Location ~\Downloads }
 Set-Alias -Name cd -Value pushd -Option AllScope
 Set-Alias -Name bd -Value popd -Option AllScope
 
+function touch($file) {
+    echo "" > $file
+}
+
 function wsl2 {
     wsl.exe -d Ubuntu-20.04
+}
+
+function weather() {
+    curl "wttr.in/sfo?n2"
+}
+
+# FIXME
+function bc-play-collection($url) {
+    curl -sS "https://bandcamp.com/ggorlen" `
+    | xmllint --html --xpath "//*[@id='pagedata']/@data-blob" 2> $null  - `
+    | sed 's/^ data-blob=\"\|\"$//g' `
+    | perl -MHTML::Entities -pe 'decode_entities($_);' `
+    | jq '{fan_id: .fan_data.fan_id, older_than_token: .wishlist_data.last_token, count: 10}' `
+    | curl -sS -X POST -H "Content-Type: Application/JSON" `
+      --data-binary "@-" https://bandcamp.com/api/fancollection/1/collection_items `
+    | jq -r .items[].item_url `
+    | xargs curl -sS `
+    | xmllint --html --xpath "//script[@data-tralbum]/@data-tralbum" 2> $null - `
+    | sed 's/^ data-tralbum=\"\|\"$//g' `
+    | perl -MHTML::Entities -pe 'decode_entities($_);' `
+    | jq -r '.trackinfo[].file."mp3-128"' `
+    | mpv -no-video --playlist=-
 }
 
